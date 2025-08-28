277a98a7acf0df0b713b62e57f693cd770bc7658
Soye Usoroh <usorohsoye@gmail.com>
2025-08-28 11:09:37 +0100

    comment out old content

--- config.js
+++ config.js
@@ -1,78 +1,80 @@
 // config.js
-const slideData = [
-  {
-    img: "images/joiners-Soye.jpg",
-    title: "New Joiners: Intern Soye and Technical support Joel",
-    desc: "We welcome Soye (IT Intern) and Joel (Technical Support) to the NLCC IT team. Their onboarding has been approved to expand end-user support capabilities to improve response times & support coverage.",
-  },
-  {
-    img: "images/banner-8.jpg",
-    title: "🖥️ New CT-Box Integration with Aria",
-    desc: "The successful integration of the new CT-Box with Varian’s Aria platform enables faster image acquisition and more seamless treatment planning.",
-  },
-  {
-    img: "images/revenue.png",
-    title: "📊 Revenue Assurance Middleware For NLCC Services",
-    desc: "To strengthen financial tracking, NLCC's IT is building a revenue assurance middleware that will sit between clinical services and the billing system.",
-  },
-  {
-    img: "https://i.ibb.co/cchghPYV/banner-3.jpg",
-    title: "📅 Survivorship Tracker",
-    desc: "The Survivorship Tracker is a custom-built module that consolidates post-treatment data for patients and supports automatic reporting.",
-  },
-  {
-    img: "images/banner-6.jpg",
-    title: "💊 e-Chemo 2.0 : Smarter Prescriptions",
-    desc: "e-Chemo 2.0 now features task progress bars and faster regimen selection to reduce delays and improve control.",
-  },
-  {
-    img: "images/banner-4.jpg",
-    title: "💻 Devices - Enterprise Scanners, Printers & Laptops",
-    desc: "Enterprise-grade printers, scanners & laptops have been deployed to enhance speed and reliability.",
-  },
-  {
-    img: "images/visits.png",
-    title: "Visits to service stations - Reappraising the paperless process",
-    desc: "Digital maturity assessments are underway to align current operations with our paperless vision.",
-  },
-  {
-    img: "images/invoice-validation.jpg",
-    title: "💉 Invoice Validation & Chemo Cycle Management",
-    desc: "This feature validates prescriptions against approved chemo cycles to reduce billing errors and maintain paperless compliance.",
-  },
-  {
-    img: "images/email-access.jpg",
-    title: "Outlook Access to Zoho Mail - Granting same experience",
-    desc: "Outlook has been configured to work seamlessly with Zoho Mail, offering familiarity with backend reliability.",
-  },
-  {
-    img: "images/cctv-room.jpg",
-    title: "Welcome to NLCC CCTV frontal coverage -Enhancing Security",
-    desc: "Front-facing CCTV now monitors key entry and patient-facing areas for round-the-clock security.",
-  },
-  {
-    img: "images/cybersec-img.jpg",
-    title: "CyberSecurity News",
-    desc: "Staff are reminded to avoid phishing emails. IT continues improving firewalls, patching, and monitoring.",
-  },
-  {
-    img: "images/password-sec.jpg",
-    title: "Email & Password security",
-    desc: "All staff must reset passwords every 90 days using strong, unique combinations not reused across platforms.",
-  },
-  {
-    img: "images/VPN-ACCESS.jpg",
-    title: "Dos and Don'ts of VPN Access",
-    desc: "Use VPN responsibly: keep credentials private, don’t use public Wi-Fi without protection, and always log out.",
-  },
-  {
-    img: "images/prjector-img.jpg",
-    title: "Tip - Projecting a laptop to Screen",
-    desc: "Connect to NLCC network, press 'Windows + P', select Duplicate or use HDMI. Call IT for help if needed.",
-  },
-  {
-    img: "images/ai-portal.png",
-    title: "Coming Soon - AI Portal",
-    desc: "NLCC's AI Portal will centralize intelligent tools across departments with seamless model integration.",
-  },
-];
+// const slideData = [
+//   {
+//     img: "images/joiners-Soye.jpg",
+//     title: "New Joiners: Intern Soye and Technical support Joel",
+//     desc: "We welcome Soye (IT Intern) and Joel (Technical Support) to the NLCC IT team. Their onboarding has been approved to expand end-user support capabilities to improve response times & support coverage.",
+//   },
+//   {
+//     img: "images/banner-8.jpg",
+//     title: "🖥️ New CT-Box Integration with Aria",
+//     desc: "The successful integration of the new CT-Box with Varian’s Aria platform enables faster image acquisition and more seamless treatment planning.",
+//   },
+//   {
+//     img: "images/revenue.png",
+//     title: "📊 Revenue Assurance Middleware For NLCC Services",
+//     desc: "To strengthen financial tracking, NLCC's IT is building a revenue assurance middleware that will sit between clinical services and the billing system.",
+//   },
+//   {
+//     img: "https://i.ibb.co/cchghPYV/banner-3.jpg",
+//     title: "📅 Survivorship Tracker",
+//     desc: "The Survivorship Tracker is a custom-built module that consolidates post-treatment data for patients and supports automatic reporting.",
+//   },
+//   {
+//     img: "images/banner-6.jpg",
+//     title: "💊 e-Chemo 2.0 : Smarter Prescriptions",
+//     desc: "e-Chemo 2.0 now features task progress bars and faster regimen selection to reduce delays and improve control.",
+//   },
+//   {
+//     img: "images/banner-4.jpg",
+//     title: "💻 Devices - Enterprise Scanners, Printers & Laptops",
+//     desc: "Enterprise-grade printers, scanners & laptops have been deployed to enhance speed and reliability.",
+//   },
+//   {
+//     img: "images/visits.png",
+//     title: "Visits to service stations - Reappraising the paperless process",
+//     desc: "Digital maturity assessments are underway to align current operations with our paperless vision.",
+//   },
+//   {
+//     img: "images/invoice-validation.jpg",
+//     title: "💉 Invoice Validation & Chemo Cycle Management",
+//     desc: "This feature validates prescriptions against approved chemo cycles to reduce billing errors and maintain paperless compliance.",
+//   },
+//   {
+//     img: "images/email-access.jpg",
+//     title: "Outlook Access to Zoho Mail - Granting same experience",
+//     desc: "Outlook has been configured to work seamlessly with Zoho Mail, offering familiarity with backend reliability.",
+//   },
+//   {
+//     img: "images/cctv-room.jpg",
+//     title: "Welcome to NLCC CCTV frontal coverage -Enhancing Security",
+//     desc: "Front-facing CCTV now monitors key entry and patient-facing areas for round-the-clock security.",
+//   },
+//   {
+//     img: "images/cybersec-img.jpg",
+//     title: "CyberSecurity News",
+//     desc: "Staff are reminded to avoid phishing emails. IT continues improving firewalls, patching, and monitoring.",
+//   },
+//   {
+//     img: "images/password-sec.jpg",
+//     title: "Email & Password security",
+//     desc: "All staff must reset passwords every 90 days using strong, unique combinations not reused across platforms.",
+//   },
+//   {
+//     img: "images/VPN-ACCESS.jpg",
+//     title: "Dos and Don'ts of VPN Access",
+//     desc: "Use VPN responsibly: keep credentials private, don’t use public Wi-Fi without protection, and always log out.",
+//   },
+//   {
+//     img: "images/prjector-img.jpg",
+//     title: "Tip - Projecting a laptop to Screen",
+//     desc: "Connect to NLCC network, press 'Windows + P', select Duplicate or use HDMI. Call IT for help if needed.",
+//   },
+//   {
+//     img: "images/ai-portal.png",
+//     title: "Coming Soon - AI Portal",
+//     desc: "NLCC's AI Portal will centralize intelligent tools across departments with seamless model integration.",
+//   },
+// ];
+
+//20-08-25
